000cecccff0caa0f7bc3ca638cfa986d0cc74624
allancodenet <allancodenet@gmail.com>
2024-04-24 21:04:02 +0300

    Add Paginator

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,11 +1,47 @@
 class BasePresenter
   @build_attributes = []
+  @relations = []
+  @sort_attributes = []
+  @filter_attributes = []
+ #metaprogramming example
+#  #CLASS_ATTRIBUTES = {
+#     build_with: :build_attributes,
+#     related_to: :relations,
+#     sort_by: :sort_attributes,
+#     filter_by: :filter_attributes
+#   }
 
+#   CLASS_ATTRIBUTES.each { |k, v| instance_variable_set("@#{v}", []) }
+
+#   class << self
+#     attr_accessor *CLASS_ATTRIBUTES.values
+
+#     CLASS_ATTRIBUTES.each do |k, v|
+#       define_method k do |*args|
+#         instance_variable_set("@#{v}", args.map(&:to_s))
+#       end
+#     end
+#   end
+ #
+ #
   class  << self
-    attr_accessor :build_attributes
-      def build_with(*args)
+    attr_accessor :relations, :sort_attributes,
+                  :filter_attributes, :build_attributes
+
+    def build_with(*args)
           @build_attributes = args.map(&:to_s)
-      end
+    end
+    def related_to(*args)
+      @relations = args.map(&:to_s)
+    end
+
+    def sort_by(*args)
+      @sort_attributes = args.map(&:to_s)
+    end
+
+    def filter_by(*args)
+      @filter_attributes = args.map(&:to_s)
+    end
   end
   
   attr_accessor :object, :params, :data
@@ -19,6 +55,11 @@ class BasePresenter
   def as_json(*)
     @data
   end
+
+
+
+
+
 
 
   
