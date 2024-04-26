b6c3d50d5289e51a1a3e14e030a6423cf7951905
allancodenet <allancodenet@gmail.com>
2024-04-26 20:37:08 +0300

    reprentation building

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,24 @@
 class ApplicationController < ActionController::API
-   rescue_from QueryBuilderError, with: :query_builder_error
+  rescue_from QueryBuilderError, with: :builder_error
+  rescue_from RepresentationBuilderError, with: :builder_error
 
-    protected
+  protected
 
-  def query_builder_error(error)
+  def builder_error(error)
     render status: 400, json: {
       error: {
+        type: error.class,
         message: error.message,
         invalid_params: error.invalid_params
       }
     }
   end
-  
-  def filter(scope)
-    Filter.new(scope, params.to_unsafe_hash).filter
-  end
-  def sort(scope)
-    Sorter.new(scope, params).sort
-  end
 
-  def paginate(scope)
-    paginator = Paginator.new(scope, request.query_parameters, current_url)
-    response.headers['Link'] = paginator.links
-    paginator.paginate
-  end
-
-  def current_url
-    request.base_url + request.path
+  def orchestrate_query(scope, actions = :all)
+    QueryOrchestrator.new(scope: scope,
+      params: params,
+      request: request,
+      response: response,
+      actions: actions).run
   end
 end
--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -3,34 +3,35 @@ class BasePresenter
   @relations = []
   @sort_attributes = []
   @filter_attributes = []
- #metaprogramming example
-#  #CLASS_ATTRIBUTES = {
-#     build_with: :build_attributes,
-#     related_to: :relations,
-#     sort_by: :sort_attributes,
-#     filter_by: :filter_attributes
-#   }
+  # metaprogramming example
+  #  #CLASS_ATTRIBUTES = {
+  #     build_with: :build_attributes,
+  #     related_to: :relations,
+  #     sort_by: :sort_attributes,
+  #     filter_by: :filter_attributes
+  #   }
 
-#   CLASS_ATTRIBUTES.each { |k, v| instance_variable_set("@#{v}", []) }
+  #   CLASS_ATTRIBUTES.each { |k, v| instance_variable_set("@#{v}", []) }
 
-#   class << self
-#     attr_accessor *CLASS_ATTRIBUTES.values
+  #   class << self
+  #     attr_accessor *CLASS_ATTRIBUTES.values
 
-#     CLASS_ATTRIBUTES.each do |k, v|
-#       define_method k do |*args|
-#         instance_variable_set("@#{v}", args.map(&:to_s))
-#       end
-#     end
-#   end
- #
- #
-  class  << self
+  #     CLASS_ATTRIBUTES.each do |k, v|
+  #       define_method k do |*args|
+  #         instance_variable_set("@#{v}", args.map(&:to_s))
+  #       end
+  #     end
+  #   end
+  #
+  #
+  class << self
     attr_accessor :relations, :sort_attributes,
-                  :filter_attributes, :build_attributes
+      :filter_attributes, :build_attributes
 
     def build_with(*args)
       @build_attributes = args.map(&:to_s)
     end
+
     def related_to(*args)
       @relations = args.map(&:to_s)
     end
@@ -43,25 +44,29 @@ class BasePresenter
       @filter_attributes = args.map(&:to_s)
     end
   end
-  
+
   attr_accessor :object, :params, :data
-  def initialize(object, params, options = {} )
-      @object = object
-      @params = params
-      @options = options
-      @data = HashWithIndifferentAccess.new
+  def initialize(object, params, options = {})
+    @object = object
+    @params = params
+    @options = options
+    @data = HashWithIndifferentAccess.new
   end
 
   def as_json(*)
     @data
   end
 
+  def build(actions)
+    actions.each { |action| send(action) }
+    self
+  end
 
+  def fields
+    FieldPicker.new(self).pick
+  end
 
-
-
-
-
-  
-  
+  def embeds
+    EmbedPicker.new(self).embed
+  end
 end
