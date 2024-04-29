69614a93d17b4a9a28621585b80c1fb368e18a94
allancodenet <allancodenet@gmail.com>
2024-04-29 16:21:40 +0300

    client authentication

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,5 +11,5 @@ class Book < ApplicationRecord
   validates :isbn_10, presence: true, length: {is: 10}, uniqueness: true
   validates :isbn_13, presence: true, length: {is: 13}, uniqueness: true
   has_one_attached :cover
-  mount_base64_uploader :cover, CoverUploader
+  # mount_base64_uploader :cover, CoverUploade
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_29_123427) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_29_125049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_29_123427) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string "key"
+    t.boolean "active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_api_keys_on_key"
   end
 
   create_table "authors", force: :cascade do |t|
